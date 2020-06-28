a3a0c5165df540adce1b5d7b2ff666798c343cb4
muffin3 <god.coder3@gmail.com>
2020-06-28 14:46:40 +0300

    routes was changed

--- app/api/views.py
+++ app/api/views.py
@@ -5,23 +5,14 @@ from flask import jsonify, request, send_file
 from genshi.template.eval import UndefinedError
 
 
-""""@api.route('/v1.01/document/', methods=['POST'])
-def document_v1():
-    if request.files['file']:
-        document = request.files['file']
-        ext = request.form['extension']
-        data = json.loads(request.form['data'])
-        return send_file(BytesIO(document.read()), attachment_filename=document.filename, as_attachment=True)"""
-
-
-@api.route('/v1.02/document/', methods=['GET'])
-def document_v2():
+@api.route('/document/', methods=['POST'])
+def document():
     try:
         if request.files.get('template') and request.files.get('data') and request.form.get('extension'):
             data = json.load(request.files['data'])
             worker = DocumentWorker(request.files['template'], request.form['extension'], data)
-            document, filename = worker.render_template()
-            return send_file(document, attachment_filename=filename, as_attachment=True)
+            document_file, filename = worker.render_template()
+            return send_file(document_file, attachment_filename=filename, as_attachment=True)
         else:
             return jsonify({'message': 'Incorrect request params!'}), 400
     except DocumentWorkerException as e:
@@ -30,3 +21,5 @@ def document_v2():
         return jsonify({'message': 'Not enough JSON params!'}), 400
     except (json.decoder.JSONDecodeError, UnicodeDecodeError):
         return jsonify({'message': 'Incorrect JSON file data!'}), 400
+    except Exception:
+        return jsonify({'message': 'Incorrect template format!'}), 400
